5e952dcc81e41a7833d10693d9bcee1649a44152
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-04-29 15:11:02 -0600

    All models added to the dbContext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,12 +2,24 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { ParkSchema } from '../models/Park.js';
+import { PostSchema } from "../models/Post.js";
+import { ReputationSchema } from "../models/Reputation.js";
+import { ThreadSchema } from "../models/Thread.js";
+import { VisitedSchema } from "../models/Visited.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 
   Park = mongoose.model('Park', ParkSchema)
+
+  Post = mongoose.model('Post', PostSchema)
+
+  Reputation = mongoose.model('Reputation', ReputationSchema)
+
+  Thread = mongoose.model('Thread', ThreadSchema)
+
+  Visited = mongoose.model('Visited', VisitedSchema)
 }
 
 export const dbContext = new DbContext()
