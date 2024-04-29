cd0332b8e0f022c6116a07f3679c18e09ed22a3e
Anne Hunt <acathunt@gmail.com>
2024-04-29 14:14:03 -0600

    env added

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-z5jvil4yi1imwszc.us.auth0.com'
+export const clientId = 'G6Y3FDqVhQMVGmXVQNjEe73HZajDLhfF'
+export const audience = 'http://racoon.com'
