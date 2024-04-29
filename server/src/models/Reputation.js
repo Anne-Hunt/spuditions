efeb937169a0e4f64fb8f4bf67bcd83115b437cc
Jarrett Cameron <native@hotmail.com>
2024-04-29 15:13:11 -0600

    Small fixes

--- server/src/models/Reputation.js
+++ server/src/models/Reputation.js
@@ -3,7 +3,7 @@ import { Schema } from "mongoose";
 
 
 export const ReputationSchema = new Schema({
-  senderId: { type: Schema.ObjectId, required: true, ref: 'Account' },
-  receiverId: { type: Schema.ObjectId, required: true, ref: 'Account' },
-  amount: { type: Number, required: true, enum: [] }
+    senderId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    receiverId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    amount: { type: Number, required: true, enum: [-1, 1] }
 }, { timestamps: true })
--- server/src/models/Visited.js
+++ server/src/models/Visited.js
@@ -3,7 +3,7 @@ import { Schema } from "mongoose";
 
 
 export const VisitedSchema = new Schema({
-  parkId: { type: Schema.ObjectId, required: true, ref: 'Park' },
-  creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
-  userRating: { type: Number, required: true, enum: [] }
+    parkId: { type: Schema.ObjectId, required: true, ref: 'Park' },
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    userRating: { type: Number, required: true, enum: [1, 2, 3, 4, 5] }
 }, { timestamps: true })
