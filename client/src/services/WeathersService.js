ee3ca61949188da7068b0e07bb8b17d35306c50a
Emma Wood <emmawood.coder@gmail.com>
2024-04-30 19:00:15 -0600

    fixed some duplicates and started on get requests

---
+++ client/src/services/WeathersService.js
@@ -0,0 +1,11 @@
+// ************************************************************************
+//					              MODIFICATION LOG
+//
+// Date Created: 4/30/2024
+// Date Last Modified: 4/30/2024
+// Last Modified By: Emma
+// 
+// #Modification Log:
+//     - 4/30/2024: Added color scheme for website into light theme
+//*************************************************************************
+
