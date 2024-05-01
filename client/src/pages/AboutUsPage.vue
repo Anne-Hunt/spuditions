29cce7cd4da5e7b5f6844f5961c97e3f0d8ada73
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-01 11:34:39 -0600

    Edits to About us page

--- client/src/pages/AboutUsPage.vue
+++ client/src/pages/AboutUsPage.vue
@@ -9,44 +9,50 @@
       <h1>Our Story</h1>
       <p>Just a group of students creating a project Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus doloremque sit dolorum accusantium asperiores aliquam numquam, alias reiciendis magnam animi dolore, sint deserunt iure perferendis quod cumque perspiciatis ea! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus doloremque sit dolorum accusantium asperiores aliquam numquam, alias reiciendis magnam animi dolore, sint deserunt iure perferendis quod cumque perspiciatis ea!</p>
     </div>
-    <h1 class="text-center mt-2">Meet the Dev team</h1>
+    <h1 class="text-center mt-2">Meet the dev team</h1>
     <hr>
-    <div class="row align-items-center px-5 mt-5">
-      <div class="col-3">
+    <div class="row align-items-center mt-5">
+
+      <div class="col-12 col-md-3 pt-2">
         <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
       </div>
-      <div class="col-9">
+      <div class="col-12 col-md-9 pb-2">
         <h1>Dwight</h1>
         <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
       </div>
-      <div class="col-9">
+      
+      <div class="col-12 col-md-9">
         <h1 class="text-end">Dwight</h1>
         <p class="fs-5 text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
       </div>
-      <div class="col-3">
+      <div class="col-12 col-md-3 text-end">
         <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
       </div>
-      <div class="col-3">
+
+      <div class="col-12 col-md-3 pt-2">
         <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
       </div>
-      <div class="col-9">
+      <div class="col-12 col-md-9 pb-2">
         <h1>Dwight</h1>
         <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
       </div>
-      <div class="col-9">
+
+      <div class="col-12 col-md-9">
         <h1 class="text-end">Dwight</h1>
         <p class="fs-5 text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
       </div>
-      <div class="col-3">
+      <div class="col-12 col-md-3 text-end">
         <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
       </div>
-      <div class="col-3">
+
+      <div class="col-12 col-md-3 pt-2">
         <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
       </div>
-      <div class="col-9">
+      <div class="col-12 col-md-9 pb-2">
         <h1>Dwight</h1>
         <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
       </div>
+
     </div>
     <div class="text-center">
       <h1>Contact Us</h1>
