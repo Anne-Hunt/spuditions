e11a4fa5b2fa2c960a5bf6893781a4d3a03c23c9
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-01 11:09:07 -0600

    Created about us page

--- client/src/pages/AboutUsPage.vue
+++ client/src/pages/AboutUsPage.vue
@@ -5,14 +5,64 @@
 
 <template>
   <section class="container-fluid">
+    <div class="text-center my-5 px-5">
+      <h1>Our Story</h1>
+      <p>Just a group of students creating a project Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus doloremque sit dolorum accusantium asperiores aliquam numquam, alias reiciendis magnam animi dolore, sint deserunt iure perferendis quod cumque perspiciatis ea! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus doloremque sit dolorum accusantium asperiores aliquam numquam, alias reiciendis magnam animi dolore, sint deserunt iure perferendis quod cumque perspiciatis ea!</p>
+    </div>
     <h1 class="text-center mt-2">Meet the Dev team</h1>
-    <div class="card">
-      
+    <hr>
+    <div class="row align-items-center px-5 mt-5">
+      <div class="col-3">
+        <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
+      </div>
+      <div class="col-9">
+        <h1>Dwight</h1>
+        <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
+      </div>
+      <div class="col-9">
+        <h1 class="text-end">Dwight</h1>
+        <p class="fs-5 text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
+      </div>
+      <div class="col-3">
+        <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
+      </div>
+      <div class="col-3">
+        <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
+      </div>
+      <div class="col-9">
+        <h1>Dwight</h1>
+        <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
+      </div>
+      <div class="col-9">
+        <h1 class="text-end">Dwight</h1>
+        <p class="fs-5 text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
+      </div>
+      <div class="col-3">
+        <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
+      </div>
+      <div class="col-3">
+        <img class="dev-img" src="https://www.myany.city/sites/default/files/styles/scaled_cropped_medium__260x260/public/field/image/node-related-images/sample-dwight-k-schrute.jpg?itok=8TfRscbA" alt="Dwight">
+      </div>
+      <div class="col-9">
+        <h1>Dwight</h1>
+        <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias numquam ab! At voluptatibus sit temporibus ratione error itaque consequatur modi cum enim possimus dolore dolorem doloribus laboriosam, aliquid aut!</p>
+      </div>
+    </div>
+    <div class="text-center">
+      <h1>Contact Us</h1>
+      <p><i class="mdi mdi-icon"></i></p>
     </div>
   </section>
 </template>
 
 
 <style lang="scss" scoped>
-
+.dev-img{
+    height: 215px;
+    width: fit-content;
+    aspect-ratio: 1/1;
+    border-radius: 50em;
+    object-fit: cover;
+    object-position: center
+  }
 </style>
