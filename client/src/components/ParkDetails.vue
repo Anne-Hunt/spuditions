6f4a5949f10f8126dcf9347909edfd19ebe04752
Tara <taraplatt88@gmail.com>
2024-05-02 16:01:39 -0700

    IGNORE POPPER CONST

--- client/src/components/ParkDetails.vue
+++ client/src/components/ParkDetails.vue
@@ -6,13 +6,6 @@ import {useFloating} from '@floating-ui/vue';
 import bootstrap from 'bootstrap';
 
 
-// vue popper const
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
-
 
 const park = computed(() => AppState.activePark)
 const activities = computed(() => AppState.activePark?.activities)
