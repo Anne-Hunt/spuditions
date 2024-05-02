2c4a1ea3d746fc42755c99608cd955d81bbdc5d0
Anne Hunt <acathunt@gmail.com>
2024-05-02 12:32:55 -0600

    get updated

--- client/src/components/ParkDetails.vue
+++ client/src/components/ParkDetails.vue
@@ -65,7 +65,6 @@ const icon = {
 					<i class="mdi mdi-star p-1"></i>
 					<i class="mdi mdi-star p-1"></i>
 					<span class="selectable ms-4">800 ratings</span>
-					<span>{{ park?.location }}</span>
 				</div>
 				<div>
 					<h5 class="mt-5 text-light">{{ park?.type }}</h5>
@@ -79,7 +78,7 @@ const icon = {
 
 		<div>
 			<iframe class="mt-4 rounded"
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.993638145866!2d-116.2892546874821!3d43.60667407098388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae494ccd9917fd%3A0xd267ec81cdf20f95!2sBoise%20CodeWorks!5e0!3m2!1sen!2sus!4v1714588860986!5m2!1sen!2sus"
+				:src="park?.GmapsLocation"
 				width="100%" height="200" style="border:0;" loading="lazy"
 				referrerpolicy="no-referrer-when-downgrade"></iframe>
 		</div>
