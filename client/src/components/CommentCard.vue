f9c5ff023a21d917a8af1526daed78be4353b29c
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-03 15:21:09 -0600

    Created components for sidebar, comment cards, and thread cards to clean up code a bit. Also created profile page shell

---
+++ client/src/components/CommentCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+
+<template>
+        <div class="col-9">
+          <div class="card bg-teal p-2 m-4">
+            <div class="row">
+            <div class="col-12 col-md-1">
+              <img class="comment-img d-inline" src="https://www.slashfilm.com/img/gallery/michael-scotts-12-most-redeeming-moments-in-the-office/l-intro-1634858598.jpg" alt="Michael">
+            </div>
+            <div class="col-10 col-sm-11 col-md-10">
+              <p class="d-inline ps-2">Profile Name</p>
+              <p class="ps-2">"Days ago"</p>
+            </div>
+            <!-- //FIXME - Add "v-if" to delete button -->
+            <div class="col-1 col-sm-1 col-md-1">
+              <button class="btn btn-danger fs-5 float-end delete-post"><i class="mdi mdi-trash-can"></i></button>
+            </div>
+            <div class="col-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus error esse ipsum nesciunt, sunt architecto! Asperiores nemo architecto ex soluta.</p>
+            </div>
+            <div class="col-12">
+              <h3 class="d-inline float-end px-3">#Tag</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+
+<style lang="scss" scoped>
+.comment-img{
+  height: 50px;
+  width: fit-content;
+  aspect-ratio: 1/1;
+  border-radius: 50em;
+  object-fit: cover;
+  object-position: center
+}
+
+.delete-post{
+  height: 45px;
+  border-radius: 50em;
+  aspect-ratio: 1/1;
+}
+</style>
