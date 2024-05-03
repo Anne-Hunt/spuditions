3561bc766b510502346a0ac2c41fec3f7271accb
Emma Wood <emmawood.coding@gmail.com>
2024-05-03 10:16:04 -0600

    added mobile view for footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -5,22 +5,27 @@
 
 <template>
 	<footer class="container-flex">
-		<div class="row bkcolor text-light align-items-center py-4 px-5 w-100 m-0">
-			<div class="col-3">
+		<div class="row bkcolor text-light p-3 py-md-4 px-md-5 w-100 m-0">
+			<div class="col-6 col-md-3 text-start">
 				<h5 class="fw-bold text-uppercase">Connect</h5>
 				<a href="/Forums" class="footlink">Forums</a><br>
 				<a href="/Search" class="footlink">Search</a><br>
 				<a href="/About" class="footlink">Contact Us</a>
 			</div>
-			<div class="col-3">
+			<div class="col-6 col-md-3">
 				<h5 class="fw-bold text-uppercase">Resources</h5>
 				<a class="footlink" href="https://www.nps.gov/index.htm">National Parks Service</a><br>
 				<a class="footlink" href="https://parksandrecreation.idaho.gov/">Idaho State Parks</a><br>
 				<a class="footlink" href="https://parksandrecreation.idaho.gov/activities/camping-tents/">Idaho
 					Campgrounds</a>
 			</div>
-			<div class="col-6 d-flex justify-content-center">
+
+			<hr class="hidden my-2 mt-3" />
+
+			<div class="col-12 col-md-6 mt-md-0 d-flex align-items-center justify-content-center justify-content-md-end">
+
 				<img src="/src/assets/img/spuditionLogoWhite.png" alt="Spuditions" class="logo">
+
 			</div>
 		</div>
 	</footer>
@@ -37,13 +42,27 @@
 	color: var(--beigeSand);
 }
 
-.logo {
-	max-height: 45px;
-}
-
 .footlink {
 	color: white;
 	font-weight: bold;
 	font-size: small;
+}
+
+.logo {
+	max-height: 80px;
+}
+
+.hidden {
+	display: none;
+}
+
+@media only screen and (max-width: 767px) {
+	.logo {
+		max-height: 60px;
+	}
+
+	.hidden {
+		display: inline;
+	}
 }
 </style>
