6523ad54624f673f58e40d112fb79a397899e1b4
Emma Wood <emmawood.coding@gmail.com>
2024-05-03 09:53:31 -0600

    added some info to park details

--- client/src/components/ParkDetails.vue
+++ client/src/components/ParkDetails.vue
@@ -9,7 +9,7 @@ import GMap from './GMap.vue';
 const park = computed(() => AppState.activePark)
 const activities = computed(() => AppState.activePark?.activities)
 
-scroll(0,0)
+scroll(0, 0)
 
 const icon = {
 	'biking': `mdi mdi-bicycle`,
@@ -137,6 +137,11 @@ async function changeVisitedStatus() {
 
 					<div class="costsBox mt-3 mt-md-5 mb-3 col-12 col-md-5">
 						<h4 class="text-center">Costs:</h4>
+						<div class="mt-1 text-center">
+							<i class="mdi mdi-information fs7 red"></i>&nbsp;
+							<i class="fs7">Non-resident charges are included on top of resident charges</i>
+							&nbsp;<i class="mdi mdi-information fs7 red"></i>
+						</div>
 						<hr>
 						<ul>
 							<li v-for="cost in park?.costs" :key="cost" class="fs-5 mb-2">
@@ -193,5 +198,9 @@ a:hover {
 
 .btn {
 	border: 3px solid white;
+}
+
+.red {
+	color: #6700ed;
 }
 </style>
