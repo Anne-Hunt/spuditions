ab99656de21fe9b4fa04c10ba5684f34df76d193
Tara <taraplatt88@gmail.com>
2024-05-05 17:36:05 -0700

    layout is better, still getting styling figured out

--- client/src/pages/ParkDetailsPage.vue
+++ client/src/pages/ParkDetailsPage.vue
@@ -65,7 +65,7 @@ onMounted(() => {
 
 
 		<!--****** SECTION: WEATHER ******-->
-		<section class="text-light container-fluid" v-if="weatherForecast">
+		<section class="container-fluid" v-if="weatherForecast">
 
 			<hr class="my-4" />
 
