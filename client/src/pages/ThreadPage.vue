d7f04aa541f2da62b6abac55958f60fb842f1608
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-07 14:13:35 -0600

    Bug fixed, now new bug

--- client/src/pages/ThreadPage.vue
+++ client/src/pages/ThreadPage.vue
@@ -1,19 +1,13 @@
 <script setup>
 import { computed, onMounted } from "vue";
-import CommentCard from "../components/CommentCard.vue";
 import Sidebar from "../components/Sidebar.vue";
 import ThreadCard from "../components/ThreadCard.vue";
 import { AppState } from "../AppState.js";
-import ModalWrap from "../components/ModalWrap.vue";
-import ThreadModal from "../components/ThreadModal.vue";
 import { threadsService } from "../services/ThreadsService.js";
 import { useRoute } from "vue-router";
 import Pop from "../utils/Pop.js";
-import PostModal from "../components/PostModal.vue";
 import { logger } from "../utils/Logger.js";
 
-const account = computed(() => AppState.account)
-const posts = computed(() => AppState.posts)
 const threads = computed(() => AppState.activeThread)
 const route = useRoute()
 
@@ -43,21 +37,10 @@ onMounted(() => {
     <div class="col-12 col-md-12 col-lg-10">
       <!-- //!SECTION - Create Post button -->
       <div class="row me-0">
-        <!-- <div class="col-12">
-          <div class="p-3 mt-3 fw-bold">
-            <h1 class="d-inline">General Chat</h1>
-            <button data-bs-toggle="modal" data-bs-target="#create-thread-modal" class="btn btn-primary rounded text-white float-end">Create Thread <i class="mdi mdi-plus"></i></button>
-            <ModalWrap modalId="create-thread-modal">
-              <ThreadModal/>
-            </ModalWrap>
-            <h5 class="py-3">discuss multiple topics</h5>
-          </div>
-        </div> -->
-
         <!-- //!SECTION - Thread card -->
         <!-- //FIXME - Need to replace info with profiles and stuff -->
         <div v-for="thread in threads" :key="thread?.id" class="col-12">
-          <ThreadCard :thread="thread" :FullView="true"/>
+          <ThreadCard :thread="threads" :fullView="true"/>
         </div>
       </div>    
   </div>
