80de83c41b8602dd25b03479c7845ea91e0852fc
Jarrett Cameron <native@hotmail.com>
2024-05-08 15:43:08 -0600

    Added padding on forum pages

--- client/src/pages/ThreadPage.vue
+++ client/src/pages/ThreadPage.vue
@@ -92,7 +92,7 @@ onMounted(() => {
     <Sidebar/>
 
     <!-- //!SECTION - Main section of the page -->
-    <div class="col-12 col-md-12 col-lg-10">
+    <div class="col-12 col-md-12 col-lg-10 mt-4">
       <!-- //!SECTION - Create Post button -->
       <div class="row me-0">
         <!-- //!SECTION - Thread card -->
