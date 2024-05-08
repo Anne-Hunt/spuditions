a399cc10ae96fd9636cce7f52362bacf709547b9
Anne Hunt <acathunt@gmail.com>
2024-05-08 17:48:09 -0600

    updated search bar in home page

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -101,27 +101,24 @@ onMounted(() => {
 
 	<section class="container-fluid bg-lightGreen py-4">
 		<!-- Adjusted container class for full-width background -->
-		<div class="row">
-			<div class="col-12 col-md-3">
-				<div class="d-flex align-items-center justify-content-center verticalLine">
-					<div class="bg-Gray rounded p-3 me-2 ms-2">
-						<div class="my-2 text-center">
-							<i class="mdi mdi-magnify fs-4 me-2"></i>
-							<span class="fs-4 me-3">Search For More</span>
-						</div>
-						<form @submit.prevent="search()">
-							<div class="form-floating mb-3">
-								<input class="form-control rounded" type="text" v-model="searchQuery" id="searchBar">
-								<!-- Added v-model for two-way binding -->
-								<label for="searchBar" class="text-light"></label>
+		<div class="row justify-content-center align-items-center">
+			<div class="my-2 text-center bg-Gray p-3 rounded col-10">
+				<form @submit.prevent="search()">
+					<div class="form-floating mb-3">
+						<input class="form-control rounded" type="text" v-model="searchQuery" id="searchBar">
+						<!-- Added v-model for two-way binding -->
+						<label for="searchBar" class="">
+									<i class="mdi mdi-magnify fs-4 me-2"></i>
+									<span class="fs-4 me-3">Search For More</span>
+
+								</label>
 							</div>
 							<!-- Adjusted button width -->
-							<button type="submit" class="btn bg-beigeSand w-100">SEARCH</button>
-						</form>
-					</div>
+						<button type="submit" class="btn bg-beigeSand w-100">SEARCH</button>
+					</form>
 				</div>
 			</div>
-			<div class="col-12 col-md-9">
+				<div class="col-12 col-md-9">
 				<h4 class="text-center fw-bold mb-5 fs-3">All Parks: (sorted A-Z)</h4>
 				<div class="row row-cols-1 row-cols-md-3 g-3">
 					<div v-for="park in parks" :key="park.id" class="col parkFont">
@@ -132,8 +129,6 @@ onMounted(() => {
 					</div>
 				</div>
 			</div>
-
-		</div>
 	</section>
 </template>
 
@@ -257,13 +252,6 @@ a:hover {
 	max-width: 100%;
 	height: 65dvh;
 }
-
-
-.verticalLine {
-	border-right: 5px solid var(--forestGreen);
-	height: 800px;
-}
-
 
 
 .hidden {
