1336f80187a7e5a1e14135329a3e04e8fc8ee9d9
Jarrett Cameron <native@hotmail.com>
2024-05-09 10:37:15 -0600

    Made login and register *less ugly*

--- client/src/pages/RegistrationPage.vue
+++ client/src/pages/RegistrationPage.vue
@@ -25,17 +25,30 @@ async function handleRegister() {
 
 
 <template>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-7 col-11">
+                <form class="mt-5 px-5" @submit.prevent="handleRegister()">
+                    <h1 class="text-center mb-3">Register</h1>
+
+                    <label>Name</label>
+                    <input v-model="formData.name" type="text" class="form-control mb-3" required>
+
+                    <label>Email</label>
+                    <input v-model="formData.email" type="email" class="form-control mb-3" required>
+
+                    <label>Password</label>
+                    <input v-model="formData.password" type="password" class="form-control mb-3" required>
+
+                    <div class="text-center">
+                        <button class="btn px-5 btn-forestGreen">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <form class="mt-5 px-5" @submit.prevent="handleRegister()">
-        <label>Name</label>
-        <input v-model="formData.name" type="text" class="form-control mb-3">
 
-        <label>Email</label>
-        <input v-model="formData.email" type="email" class="form-control mb-3">
-
-        <label>Password</label>
-        <input v-model="formData.password" type="password" class="form-control mb-3">
-
-        <button class="btn px-5 btn-forestGreen">Register</button>
     </form>
 </template>
 
