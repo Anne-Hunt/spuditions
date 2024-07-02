161cfec2f65ff7f850637efe1c6a21c24bb3634c
Anne Hunt <acathunt@gmail.com>
2024-07-01 22:14:28 -0600

    updated

--- client/src/env.js
+++ client/src/env.js
@@ -1,3 +1,3 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : 'https://www.spuditions.com'
 export const useSockets = false
